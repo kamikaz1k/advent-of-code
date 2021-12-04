e0161b914c83f23f6d0283d0b380f9dbde9ae8c0
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2021-12-04 16:28:37 -0500

    finished up day4

---
+++ 2021/template.py
@@ -0,0 +1,33 @@
+q1 = '''
+'''
+
+q2 = '''
+'''
+
+
+def get_input(string):
+    return [
+        i
+        for i in string.strip().split('\n')
+    ]
+
+
+def run(problem, arg, expected=None):
+    print(problem.__name__, 'result:', problem(arg), 'expected: ' + str(expected) if expected else None)
+
+
+def problem_one(vals):
+    pass
+
+
+def problem_two(vals):
+    pass
+
+
+if __name__ == '__main__':
+
+    run(problem_one, get_input(q1))
+    # run(problem_one, get_input(q2))
+
+    # run(problem_two, get_input(q1))
+    # run(problem_two, get_input(q2))
