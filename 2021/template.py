6bad544e6db6fad5aa1913de4dc1e13e9183c7e8
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2021-12-13 00:59:47 -0500

    use tester helper for running solutions

--- 2021/template.py
+++ 2021/template.py
@@ -1,19 +1,12 @@
+import time
+from collections import defaultdict, deque, Counter
+
+
 q1 = '''
 '''
 
 q2 = '''
 '''
-
-from collections import defaultdict, deque, Counter
-
-
-def log(*args):
-    # return
-    print(*args)
-
-
-def boldify(val):
-    return '\033[1m' + f"{val}" + '\033[0m'
 
 
 def get_input(string):
@@ -23,8 +16,49 @@ def get_input(string):
     ]
 
 
-def run(problem, arg, expected=None):
-    print(problem.__name__, 'result:', problem(arg), 'expected: ' + str(expected) if expected else None)
+LOG = True
+
+
+def log(*args):
+    if not LOG:
+        return
+    print(*args)
+
+
+def boldify(val):
+    return '\033[1m' + f"{val}" + '\033[0m'
+
+
+def tester(arg, expected, method, debug=0, show_input_on_fail=True):
+    global LOG
+
+    OKGREEN = '\033[92m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    # HEADER = '\033[95m'
+    # OKBLUE = '\033[94m'
+    # WARNING = '\033[93m'
+    # BOLD = '\033[1m'
+    # UNDERLINE = '\033[4m'
+    LOG = bool(debug)
+
+    start = time.time()
+    actual = method(arg)
+
+    log("hi!")
+
+    duration = time.time() - start
+    result = actual == expected
+    print("Pass: ", OKGREEN if result else FAIL, result, ENDC, " time: ", duration, " Actual: ", actual, " Expected: ", expected)#, " Input: ", input)
+
+    if show_input_on_fail and not result:
+        print("Failed for input: ", arg)
+
+
+def run(problem, arg, expected=None, debug=None):
+    if debug is None:
+        debug = LOG
+    tester(arg, expected, problem, debug=debug)
 
 
 def problem_one(vals):
@@ -37,7 +71,7 @@ def problem_two(vals):
 
 if __name__ == '__main__':
 
-    run(problem_one, get_input(q1))
+    run(problem_one, get_input(q1), debug=True)
     # run(problem_one, get_input(q2))
 
     # run(problem_two, get_input(q1))
